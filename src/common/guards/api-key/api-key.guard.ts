d8b5e0bbf3141a6fc71de2dfa6f95155754f8dd0
SNA Rahanet <navidjan.azimi132@gmail.com>
2026-01-07 11:45:13 +0430

    guards are done

---
+++ src/common/guards/api-key/api-key.guard.ts
@@ -0,0 +1,12 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class ApiKeyGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    const apiKey = request.headers['x-api-key'];
+    console.log(apiKey);
+    return apiKey === 'secret123';
+  }
+}
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { ApiKeyGuard } from '../common/guards/api-key/api-key.guard';
 
 @Controller('products')
+@UseGuards(ApiKeyGuard)
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
   @Get()
